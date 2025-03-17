9c5014d2bc7724e6c9ad88636e5d6f009e3be355
Ibrahim6231 <ibrahimkhan938@gmail.com>
2025-03-17 16:59:29 +0530

    config optimised

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,5 +4,4 @@ config();
 export const envConfig = {
     JWT_SECRET: process.env.JWT_SECRET || "chatbot-app-scerectKey",
     KEY_M: process.env.KEY_M || "",
-    FRONTEND_URL: process.env.FRONTEND_URL
 } 
